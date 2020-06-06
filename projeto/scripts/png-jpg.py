983a70f19ed58f22d4cc53c70083b06c65b202e3
paulogsc <paulogscomasetto@gmail.com>
2020-06-05 21:39:42 -0300

    :heavy_plus_sign: Adicionado script de conversao png->jpg

---
+++ projeto/scripts/png-jpg.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+from os import path
+from subprocess import call
+
+# obtendo argumentos da linha de comando
+
+psr = argparse.ArgumentParser(description="""
+    Script para converter arquivos .png para .jpg.
+""")
+
+psr.add_argument("p", help="Caminho dos arquivos.")
+psr.add_argument("-q", default=100, type=int, help="Qualidade do jpeg (default=100).")
+
+args = psr.parse_args()
+
+p = args.p
+q = args.q
+
+# normalizando paths
+
+p = path.abspath(path.expanduser(p))
+
+# convertendo arquivos
+
+os.chdir(p)
+
+com1 = "mogrify -format jpg -quality {} *.png".format(q)
+call(com1, shell=True)
+
+# excluindo arquivos .png
+
+com2 = "rm *.png"
+call(com2, shell=True)
