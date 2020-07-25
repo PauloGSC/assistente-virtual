13023768fa6fcc49c2cbb4ea1e74246853ebec9b
paulogsc <paulogscomasetto@gmail.com>
2020-07-24 23:27:13 -0300

    :hammer_and_wrench: Removendo requirements e movendo scripts

--- projeto/outros/requirements/tools.sh
+++
@@ -1,9 +0,0 @@
-# requerimentos para executar as tarefas relacionadas ao dataset do projeto
-# manipulação de imagens e labels
-
-# programa para rotular imagens
-pip3 install --upgrade labelImg
-# programa para operações/transformações com arquivos de imagem
-sudo apt install -y imagemagick
-# programa para visualização e inspeção de imagens
-sudo apt install -y geeqie
--- projeto/outros/requirements/update.sh
+++
@@ -1,8 +0,0 @@
-# script para atualizar pip e apt
-# rodar antes de qualquer outro
-
-sudo apt -y update
-sudo apt -y upgrade
-
-sudo apt install -y python3-pip
-pip3 install --upgrade pip
