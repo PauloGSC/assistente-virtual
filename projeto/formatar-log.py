e4219dca95115b16194f1e7ddb521d9426911d4c
paulogsc <paulogscomasetto@gmail.com>
2020-12-05 17:17:38 -0300

    :wrench: Recebendo tambem diretorio como entrada / corrigido bug no formatar-log.py

--- projeto/formatar-log.py
+++ projeto/formatar-log.py
@@ -1,11 +1,13 @@
 import argparse
 from collections import defaultdict as defdict
+from glob import glob
 import csv
 import os, os.path as path
 
 
 psr = argparse.ArgumentParser(description="""
     Script para formatar logs de detecções realizadas pelo modelo.
+    Recebe como parâmetro um arquivo ou um diretório contendo os arquivos TXT.
     O log deve estar no formato original gerado pelo modelo:
         <n-frame> <id-classe> <conf> <localizacao>
     O log formatado possuirá o seguinte formato (em CSV):
@@ -13,7 +15,7 @@ psr = argparse.ArgumentParser(description="""
     formatter_class=argparse.RawDescriptionHelpFormatter
 )
 
-psr.add_argument("p", help="Caminho do arquivo de log.")
+psr.add_argument("p", help="Caminho do arquivo de log/diretório.")
 
 args = psr.parse_args()
 
@@ -21,79 +23,99 @@ args = psr.parse_args()
 
 p = path.abspath(path.expanduser(args.p))
 
-# obtém o conteúdo do arquivo de entrada
+# verifica se é diretório ou arquivo
 
-arq_entrada = open(p)
-entrada = arq_entrada.read().splitlines()
-arq_entrada.close()
+entradas = []
+if path.isdir(p):
+    entradas = glob(path.join(p, "*.txt"))
+elif path.isfile(p):
+    entradas = [p]
+else:
+    print("Diretório/arquivo inválido")
+entradas.sort()
 
-# 1) extrai as classes presentes no log
+# itera sobre a lista de arquivos de entrada
 
-classes = set()
-for linha in entrada:
-    cls = int(linha.split()[1])
-    classes.add(cls)
-classes = list(classes)
+for arq in entradas:
 
-# 2) transformando o arquivo de log em um dicionário
-# - o dicionário é orientado aos frames, e não linhas
+    # obtém o conteúdo do arquivo de entrada
 
-dicio = defdict(list)
+    arq_entrada = open(arq)
+    entrada = arq_entrada.read().splitlines()
+    arq_entrada.close()
 
-for linha in entrada:
-    n_frame = int(linha.split()[0])
-    id_classe = int(linha.split()[1])
+    # 1) extrai as classes presentes no log
 
-    dicio[n_frame].append(id_classe)
+    classes = set()
+    for linha in entrada:
+        cls = int(linha.split()[1])
+        classes.add(cls)
+    classes = list(classes)
+    classes.sort()
 
-# 3) iterando sobre o dicionário, e gerando logs formatados
+    # 2) transformando o arquivo de log em um dicionário
+    # - o dicionário é orientado aos frames, e não linhas
 
-logs = []
+    dicio = defdict(list)
 
-frames = list(sorted(dicio.keys()))
-for classe in classes:
-    # log: <n_frame>,<id_classe>,<total_adjacentes>
-    log_atual = [-1, classe, -1]
+    for linha in entrada:
+        n_frame = int(linha.split()[0])
+        id_classe = int(linha.split()[1])
 
-    for i, n_frame in enumerate(frames):
-        if i == 0 or n_frame == frames[i-1]+1: # possível continuidade
-            if classe in dicio[n_frame]: # continua ou recomeça
-                if log_atual[2] == -1: # recomeça
-                    log_atual[0] = n_frame # grava nº do frame
+        dicio[n_frame].append(id_classe)
 
-                # de qualquer modo, incrementa contador de adjacências
-                log_atual[2] += 1
+    # 3) iterando sobre o dicionário, e gerando logs formatados
 
-            else: # interrompe possível sequência (objeto ausente)
-                if log_atual[2] > -1: # havia uma adjacência em andamento
+    logs = []
+
+    frames = list(sorted(dicio.keys()))
+    for classe in classes:
+        # log: <n_frame>,<id_classe>,<total_adjacentes>
+        log_atual = [-1, classe, -1]
+
+        for i, n_frame in enumerate(frames):
+            if i == 0 or n_frame == frames[i-1]+1: # possível continuidade
+                if classe in dicio[n_frame]: # continua ou recomeça
+                    if log_atual[2] == -1: # recomeça
+                        log_atual[0] = n_frame # guarda nº do frame
+
+                    # de qualquer modo, incrementa contador de adjacências
+                    log_atual[2] += 1
+
+                else:
+                    if log_atual[2] > -1: # interrompe por ausência da classe
+                        logs.append(log_atual.copy())
+                        log_atual = [-1, classe, -1] # reseta
+
+            else: # quebra na continuidade dos frames
+                if log_atual[2] > -1: # interrompe por pulo entre frames
                     logs.append(log_atual.copy())
                     log_atual = [-1, classe, -1] # reseta
 
-        else: # interrompe possível sequência (pulo entre frames)
-            if log_atual[2] > -1: # havia uma adjacência em andamento
-                logs.append(log_atual.copy())
-                log_atual = [-1, classe, -1] # reseta
+                if classe in dicio[n_frame]: # recomeço
+                    log_atual[0] = n_frame # guarda nº do frame
+                    log_atual[2] += 1 # incrementa contador
 
-    # verifica se havia uma última adjacência pendente
-    if log_atual[2] > -1: # havia uma adjacência em andamento
-        logs.append(log_atual.copy())
-        log_atual = [-1, classe, -1] # reseta, supérfluo
+        # verifica se havia uma última adjacência pendente
+        if log_atual[2] > -1: # havia uma adjacência em andamento
+            logs.append(log_atual.copy())
+            log_atual = [-1, classe, -1] # reseta, supérfluo
 
-# 4) ordena os logs
+    # 4) ordena os logs
 
-logs.sort()
+    logs.sort()
 
-# 5) escreve os logs no CSV de saída
+    # 5) escreve os logs no CSV de saída
 
-# caminho do arquivo CSV de saída
+    # caminho do arquivo CSV de saída
 
-caminho_saida = path.splitext(p)[0] + ".csv"
+    caminho_saida = path.splitext(arq)[0] + ".csv"
 
-# escrevendo no arquivo de saída
+    # escrevendo no arquivo de saída
 
-arq_saida = open(caminho_saida, "w", newline="")
-escritor = csv.writer(arq_saida)
+    arq_saida = open(caminho_saida, "w", newline="")
+    escritor = csv.writer(arq_saida)
 
-escritor.writerows(logs)
+    escritor.writerows(logs)
 
-arq_saida.close()
+    arq_saida.close()
